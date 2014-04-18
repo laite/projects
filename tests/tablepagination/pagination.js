95095abf81094e7fb131550830a62b287abe176b
Markus Klinga <laite@gmx.com>
2014-04-18 20:10:39 +0300

    finishing touches

--- tests/tablepagination/pagination.js
+++ tests/tablepagination/pagination.js
@@ -1,3 +1,16 @@
+/*
+ *
+ * Table paginatio script
+ *
+ * Author: Markus Klinga 2014
+ * Licence: Public domain
+ *
+ */
+
+
+/* id of the table to paginate */
+var tableIdName = 'table';
+
 var headers = "";
 var tableData = [];
 var currentPage = 0;
@@ -6,13 +19,14 @@ var itemsPerPage = 5;
 function populateTable() {
 
 	/* empty current table and append headers */
-	$('table').empty().append(headers);
+	$(document.getElementById(tableId)).empty().append(headers);
 
-	/* Load max items to table */
+	/* Calculate the amount of items visible */
 	var amount = Math.min(tableData.length - (currentPage*itemsPerPage), itemsPerPage);
 
+	/* Add items to actual table */
 	for (var i = 0; i < amount; i++) {
-		$('table').append(tableData[(currentPage*itemsPerPage) + i]);
+		$(document.getElementById(tableId)).append(tableData[(currentPage*itemsPerPage) + i]);
 	}
 }
 
@@ -20,7 +34,6 @@ function changePage(pageNum) {
 	currentPage = pageNum;
 
 	$('#currentPage').html("Page " + (1 + currentPage) + " of " + Math.ceil(tableData.length/itemsPerPage));
-	populateTable();
 
 	if (currentPage === 0)
 		$('#prevPage').addClass('disabled');
@@ -31,36 +44,32 @@ function changePage(pageNum) {
 		$('#nextPage').addClass('disabled');
 	else
 		$('#nextPage').removeClass('disabled');
+
+	populateTable();
 }
 
 $(document).ready(function() {
 
 	/* Load existing data from table to variable */
 	$('tr').each(function() {
-		if ($(this).find('th').html()) {
+		if ($(this).find('th').html())
 			headers = "<tr>" + $(this).html() + "</tr>";
-		}
-		else {
+		else
 			tableData.push("<tr>" + $(this).html() + "</tr>");
-		}
 	});
 
 	$('#nextPage').click(function() {
-		if (tableData.length <= (currentPage+1)*itemsPerPage)
-			return;
-
-		changePage(currentPage+1);
+		if (tableData.length > (currentPage+1)*itemsPerPage)
+			changePage(currentPage+1);
 	});
 
 	$('#prevPage').click(function() {
-		if (currentPage === 0)
-			return;
-
-		changePage(currentPage-1);
+		if (currentPage !== 0)
+			changePage(currentPage-1);
 	});
 
 	/* Set initial page and show table */
-	changePage(0);
-	$('table').show();
+	changePage(currentPage);
+	$(document.getElementById(tableId)).show();
 
 });
