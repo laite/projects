07f3a42679bba65af7bb7c0143fc26fb77eeffa1
Markus Klinga <laite@gmx.com>
2014-04-24 20:01:13 +0300

    generalize pagination function to be used as a "class"

--- tests/tablepagination/pagination.js
+++ tests/tablepagination/pagination.js
@@ -8,68 +8,75 @@
  */
 
 
-/* id of the table to paginate */
-var tableIdName = 'table';
+var Table = function(tableId) {
 
-var headers = "";
-var tableData = [];
-var currentPage = 0;
-var itemsPerPage = 5;
+	this.id = tableId;
 
-function populateTable() {
+	this.headers = "";
+	this.tableData = [];
+	this.currentPage = 2;
+	this.itemsPerPage = 5;
 
-	/* empty current table and append headers */
-	$(document.getElementById(tableIdName)).html(headers);
-
-	/* Calculate the amount of items visible */
-	var amount = Math.min(tableData.length - (currentPage*itemsPerPage), itemsPerPage);
-
-	/* Add items to actual table */
-	for (var i = 0; i < amount; i++) {
-		$(document.getElementById(tableIdName)).append(tableData[(currentPage*itemsPerPage) + i]);
-	}
-}
-
-function changePage(pageNum) {
-	currentPage = pageNum;
-
-	$('#currentPage').html("Page " + (1 + currentPage) + " of " + Math.ceil(tableData.length/itemsPerPage));
-
-	if (currentPage === 0)
-		$('#prevPage').addClass('disabled');
-	else
-		$('#prevPage').removeClass('disabled');
-
-	if (tableData.length <= (currentPage+1)*itemsPerPage)
-		$('#nextPage').addClass('disabled');
-	else
-		$('#nextPage').removeClass('disabled');
-
-	populateTable();
-}
-
-$(document).ready(function() {
+	/* Reference to instance to be used below at .each-function */
+	var that = this;
 
 	/* Load existing data from table to variable */
-	$('tr').each(function() {
+	$("#" + that.id + " tr").each(function() {
 		if ($(this).find('th').html())
-			headers = "<tr>" + $(this).html() + "</tr>";
+			that.headers = "<tr>" + $(this).html() + "</tr>";
 		else
-			tableData.push("<tr>" + $(this).html() + "</tr>");
+			that.tableData.push("<tr>" + $(this).html() + "</tr>");
 	});
 
-	$('#nextPage').click(function() {
-		if (tableData.length > (currentPage+1)*itemsPerPage)
-			changePage(currentPage+1);
-	});
+// 	$('#nextPage').click(function() {
+// 		if (tableData.length > (currentPage+1)*itemsPerPage)
+// 			changePage(currentPage+1);
+// 	});
+//
+// 	$('#prevPage').click(function() {
+// 		if (currentPage !== 0)
+// 			changePage(currentPage-1);
+// 	});
 
-	$('#prevPage').click(function() {
-		if (currentPage !== 0)
-			changePage(currentPage-1);
-	});
+	this.populateTable = function() {
+
+		/* empty current table and append headers */
+		$(document.getElementById(this.id)).html(this.headers);
+
+		/* Calculate the amount of items visible */
+		var amount = Math.min(this.tableData.length - (this.currentPage*this.itemsPerPage), this.itemsPerPage);
+
+		/* Add items to actual table */
+		for (var i = 0; i < amount; i++) {
+			$(document.getElementById(this.id)).append(this.tableData[(this.currentPage*this.itemsPerPage) + i]);
+		}
+	};
+
+	this.changePage = function(pageNum) {
+		this.currentPage = pageNum;
+
+		$('#currentPage').html("Page " + (1 + this.currentPage) + " of " + Math.ceil(this.tableData.length/this.itemsPerPage));
+
+		if (this.currentPage === 0)
+			$('#prevPage').addClass('disabled');
+		else
+			$('#prevPage').removeClass('disabled');
+
+		if (this.tableData.length <= (this.currentPage+1)*this.itemsPerPage)
+			$('#nextPage').addClass('disabled');
+		else
+			$('#nextPage').removeClass('disabled');
+
+		this.populateTable();
+	};
 
 	/* Set initial page and show table */
-	changePage(currentPage);
-	$(document.getElementById(tableIdName)).show();
+	this.changePage(this.currentPage);
+	$(document.getElementById(this.id)).show();
 
+};
+
+$(window).ready(function() {
+	window.myTable1 = new Table('table1');
+	window.myTable2 = new Table('table2');
 });
