30179c8c9c32a2d0b126d8d84a5defede8b9c47a
Markus Klinga <laite@gmx.com>
2014-05-29 18:20:40 +0300

    move timetracker to its own repository

--- timetracker/client/scripts/collections/history.js
+++
@@ -1,7 +0,0 @@
-define([ 'underscore', 'backbone', 'models/history' ], function(_, Backbone, HistoryModel){
-	var HistoryCollection = Backbone.Collection.extend({
-		model: HistoryModel
-	});
-
-	return HistoryCollection;
-});
--- timetracker/client/scripts/collections/project.js
+++
@@ -1,7 +0,0 @@
-define([ 'underscore', 'backbone', 'models/history' ], function(_, Backbone, ProjectModel){
-	var ProjectCollection = Backbone.Collection.extend({
-		model: ProjectModel
-	});
-
-	return ProjectCollection;
-});
