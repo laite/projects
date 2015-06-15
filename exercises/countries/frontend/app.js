c84ebd96964e20d60d3f0112ad0b4ed9d170430c
Markus Klinga <markus.klinga@sc5.io>
2015-06-15 10:24:51 +0300

    Add initial functionality

--- exercises/countries/frontend/app.js
+++ exercises/countries/frontend/app.js
@@ -1,7 +1,26 @@
 
 angular.module('country', [])
-    .controller('countryController', ['$scope', function($scope) {
-        $scope.countries = [{ 'name': 'Suomi' }, { 'name': 'Ruotsi'}];
+
+    .factory('countryList', ['$http', function($http) {
+        return function() {
+            return $http.get('http://localhost/countryapi/public/countries');
+        };
+    }])
+
+    .controller('countryController', ['$scope', 'countryList', function($scope, CountryList) {
+
+        // find by name or code based on the search box
+        $scope.nameOrCodeMatch =  function(data) {
+            if (!$scope.countrySearch) return true;
+
+            return ($scope.countrySearch === data.name || $scope.countrySearch === data.code);
+        };
+
+        CountryList()
+            .success(function(data) {
+                $scope.countries = data;
+            });
+
         $scope.countrySearch = "";
     }]);
-    
+
