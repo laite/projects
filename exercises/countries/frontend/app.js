82d1ffd0bf90dd86d886e45e96a007bd31c963a8
Markus Klinga <markus.klinga@sc5.io>
2015-06-15 13:28:01 +0300

    Separate the filter from controller

--- exercises/countries/frontend/app.js
+++ exercises/countries/frontend/app.js
@@ -1,6 +1,24 @@
 
 angular.module('country', [])
 
+    .filter('nameOrCodeMatch', function() {
+
+        function nameOrCodeFilter(data, searchWord) {
+            if (!searchWord) return data;
+
+            data = data.filter(function(item) {
+                return ((item.name === searchWord) || (item.code === searchWord));
+            });
+
+            return data;
+        }
+
+        nameOrCodeFilter.$stateful = true;
+
+        return nameOrCodeFilter;
+
+    })
+
     .factory('countryList', ['$http', function($http) {
         return function() {
             return $http.get('http://localhost/countryapi/public/countries');
@@ -8,13 +26,6 @@ angular.module('country', [])
     }])
 
     .controller('countryController', ['$scope', 'countryList', function($scope, CountryList) {
-
-        // find by name or code based on the search box
-        $scope.nameOrCodeMatch =  function(data) {
-            if (!$scope.countrySearch) return true;
-
-            return ($scope.countrySearch === data.name || $scope.countrySearch === data.code);
-        };
 
         CountryList()
             .success(function(data) {
