3d0de5eb6807504b08ba9b31a4da0720fda52dc1
Markus Klinga <markus.klinga@sc5.io>
2015-06-15 13:59:43 +0300

    Fetch country details

--- exercises/countries/frontend/app.js
+++ exercises/countries/frontend/app.js
@@ -13,6 +13,7 @@ angular.module('country', [])
             });
         }
 
+        // We need to run this on every change since it depends on the countries - list
         nameOrCodeFilter.$stateful = true;
 
         return nameOrCodeFilter;
@@ -25,13 +26,44 @@ angular.module('country', [])
         };
     }])
 
-    .controller('countryController', ['$scope', 'countryList', function($scope, CountryList) {
+    .factory('countryDetail', ['$http', function($http) {
+        return function(id) {
+            return $http.get('http://localhost/countryapi/public/countries/' + id);
+        };
+    }])
+
+    .directive('countryDetail', function() {
+        return {
+            restrict: 'E',
+            template: '<div>' +
+                '<h2>{{ selectedCountry.name }}</h2>' + 
+                '<ul>' +
+                '<li> Capital: {{ selectedCountry.capital }} </li>' +
+                '<li> Population: {{ selectedCountry.population }} </li>' +
+                '</ul></div>',
+            link: function(scope, element, attrs) {
+                console.log(scope.selectedCountry);
+            }
+        };
+    })
+
+    .controller('countryController', ['$scope', 'countryList', 'countryDetail', function($scope, CountryList, CountryDetail) {
+
+        $scope.countrySearch = "";
+        $scope.selectedCountry = {};
 
         CountryList()
             .success(function(data) {
                 $scope.countries = data;
             });
 
-        $scope.countrySearch = "";
+        $scope.selectCountry = function (country) {
+            CountryDetail(country.id)
+                .success(function(data) {
+                    data.name = country.name; // our API doesn't provide country name on detailed information
+                    $scope.selectedCountry = data;
+                });
+        };
+
     }]);
 
