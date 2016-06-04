e743f7f4fcdddaf6119606df0d86dd2203edec42
Markus Klinga <laite@gmx.com>
2016-06-04 19:04:52 +0300

    Fix tabs

--- java/exercises4/LabeledPoint.java
+++ java/exercises4/LabeledPoint.java
@@ -2,16 +2,16 @@ package exercises4;
 
 public class LabeledPoint extends Point {
 
-	private String label;
+  private String label;
 
   public LabeledPoint(String label, double x, double y) { 
     super(x, y);
-		this.label = label;
+    this.label = label;
   }
 
-	public String getLabel() {  
-		return label;
-	}
+  public String getLabel() {  
+    return label;
+  }
 
   @Override
   public String toString() {
